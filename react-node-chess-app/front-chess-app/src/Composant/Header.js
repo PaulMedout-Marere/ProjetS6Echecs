8df8a3dc1b0b1dd0feb8a46e2e9d47334d6ff2ce
Elliot-mazerand <elliotmazerand@gmail.com>
2023-02-17 19:21:10 +0100

    incr

--- react-node-chess-app/front-chess-app/src/Composant/Header.js
+++ react-node-chess-app/front-chess-app/src/Composant/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
-import './chess.png';
+import chess from './chess.png';
 
 function Header(){
     return (
         <div className='Header'>
             <h1 className='titre'> Blind Chess </h1>
-            <img className='chess' src='https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/CHESScom/phpyyRnHb.png' alt='imgchess'></img>
+            <img className='chess' src={chess} alt='imgchess'></img>
             
         </div>
     )
